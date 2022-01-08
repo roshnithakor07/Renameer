63f7de9cefdb95d8f4a404f0843e31b81f4f9211
Roshni Thakor <74705502+roshnithakor07@users.noreply.github.com>
2022-01-08 21:55:00 +0530

    Add files via upload

---
+++ renameer/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+let path_of_directory = __dirname + path.join("/roshni/");
+
+// files inside folder or directory
+let files = fs.readdirSync(path_of_directory);
+
+files.forEach((e, index) => {
+  fs.renameSync(
+    path_of_directory + e,
+    path_of_directory + `read- ${index + 1}.py`
+    
+  );
+});
